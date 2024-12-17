32e8ba1088b31bbb0b8094ec880b39ca3d617d64
prathap <prathapyadavmiriyam02@gmail.com>
2024-12-17 16:30:10 +0530

    Book.java

---
+++ Book.java
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book Store</title>
+</head>
+<body bgcolor="#bb3cd">
+    <center><b>Books Available</b></center><br>
+    <table border="5" bordercolor="#bce253" cellspacing="2" cellpadding="2">
+        <tr>
+            <th>Book Title</th>
+            <th>Author</th>
+            <th>Edition</th>
+            <th>Publication</th>
+            <th>Price</th>
+            <th>Add to Cart</th>
+        </tr>
+        <tr>
+            <td>Software Engineering</td>
+            <td>Ian Summerville</td>
+            <td>8th</td>
+            <td>Pearson Education</td>
+            <td>$1.9</td>
+            <td><input type=button value="Add to Cart"></td>
+        </tr>
+        <tr>
+            <td>System Software</td>
+            <td>Donovan</td>
+            <td>2nd</td>
+            <td>Tata McGraw-Hill</td>
+            <td>$1.4</td>
+            <td><input type=button value="Add to Cart"></td>
+        </tr>
+        <tr>
+            <td>Advanced Java for Web Technologies</td>
+            <td>Patrick Naughton and Herbert Schildt</td>
+            <td>2nd</td>
+            <td>SPD</td>
+            <td>$2.5</td>
+            <td><input type=button value="Add to Cart"></td>
+        </tr>
+        <tr>
+            <td>Data Warehousing and Data Mining</td>
+            <td>Jiawei Han and Micheline Kamber</td>
+            <td>2nd</td>
+            <td>Morgan Kaufmann</td>
+            <td>$1.2</td>
+            <td><input type=button value="Add to Cart"></td>
+        </tr>
+        <tr>
+            <td>Cloud Computing</td>
+            <td>Michael Miller</td>
+            <td>5th</td>
+            <td>Pearson Education</td>
+            <td>$1.6</td>
+            <td><input type=button value="Add to Cart"></td>
+        </tr>
+    </table>
+</body>
+</html>
